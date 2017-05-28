e3aa9a47496eaaca03e8f6e29fd6aa23bb69856e
Wafflenaut <moyerjo@oregonstate.edu>
2017-05-28 19:42:07 -0400

    Fixed issue in scraper to allow for >25 records

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var schedule = require('node-schedule');
 
 //John's API Key for Indeed.com and our max scraping number
 var indeedAPI = "2648850467422389";
-var jobsPerPage = 20;
+var jobsPerPage = 25;
 
 var job = schedule.scheduleJob('* * /12 * * *', function(){
 	//runs the code here every 12 hours
@@ -29,54 +29,7 @@ var job = schedule.scheduleJob('* * /12 * * *', function(){
 
 var app = express();
 
-function scrapeIndeed(){
-	/*var url = "http://api.indeed.com/ads/apisearch?publisher=" + indeedAPI + "&format=json"
-	+ "&l=remote" + "&start=0" + "&limit=" + jobsPerPage + "&co=us&userip=1.2.3.4"
-		+ "&useragent=Mozilla/%2F4.0%28Firefox%29" + "v=2";*/
-	
-var url = "http://api.indeed.com/ads/apisearch?publisher=" + indeedAPI + "&format=json&l=remote&sort=&radius=&st=&jt=&start=&limit=" + jobsPerPage + "&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
-	
-	request.get(url, function(error, indRes, indRet){
-		
-		var jobs = JSON.parse(indRet);
-		
-		console.log(jobs.results[0]);
-			
-		//drops table if it exists then creates new one
-		var context = {};
-		  mysql.pool.query("DROP TABLE IF EXISTS indeedJobs", function(err){
-			var createString = "CREATE TABLE indeedJobs(" +
-			"id INT PRIMARY KEY AUTO_INCREMENT," +
-			"jobTitle VARCHAR(255) NOT NULL," +
-			"company VARCHAR(255) NOT NULL," + 
-			"daysAgo VARCHAR(255) NOT NULL,"+
-			"url TEXT NOT NULL, " +
-			"snippet TEXT NOT NULL)";
-			mysql.pool.query(createString, function(err){
-			  context.results = "Job Table reset";
-			  res.setHeader('Content-Type', 'application/json');
-			  res.send(context);
-			})
-		  });
-		
-		for(i = 0; i < jobsPerPage; i++){
-			console.log("Current Job: " + i)
-			//just for testing
-			console.log("jobtitle: " + jobs.results[i].jobtitle);
-				// disabled mysql due to errors
-				/*mysql.pool.query("INSERT INTO indeedJobs (jobTitle, company, daysAgo, snippet) VALUES (?, ?, ?, ?, ?)", [jobs.results[i].jobtitle, jobs.results[i].company, jobs.results[i].formattedRelativeTime, jobs.results[i].url, jobs.results[i].snippet],
-				function(err, result){
-					if(err){
-						next(err);
-						return;
-					}
-			
-				});*/
-			}
-	});
-}
 
-scrapeIndeed();
 
 
 
@@ -108,9 +61,64 @@ app.set('view cache', false);
 app.use('/', routes);
 app.use('/user', user);
 
-
+function scrapeIndeed(){
+	/*var url = "http://api.indeed.com/ads/apisearch?publisher=" + indeedAPI + "&format=json"
+	+ "&l=remote" + "&start=0" + "&limit=" + jobsPerPage + "&co=us&userip=1.2.3.4"
+		+ "&useragent=Mozilla/%2F4.0%28Firefox%29" + "v=2";*/
+var startJob = 0;
+		
+var url = "http://api.indeed.com/ads/apisearch?publisher=" + indeedAPI + "&format=json&l=remote&sort=&radius=&st=&jt=&start=" + startJob + "&limit=" + jobsPerPage + "&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2";
+	
+	request.get(url, function(error, indRes, indRet){
+		
+		var jobs = JSON.parse(indRet);
+		
+		console.log(jobs.results[0]);
+		var currentMaxJob = jobs.totalResults;	
+		//drops table if it exists then creates new one
+		var context = {};
+		  mysql.pool.query("DROP TABLE IF EXISTS indeedJobs", function(err){
+			var createString = "CREATE TABLE indeedJobs(" +
+			"id INT PRIMARY KEY AUTO_INCREMENT," +
+			"jobTitle VARCHAR(255) NOT NULL," +
+			"company VARCHAR(255) NOT NULL," + 
+			"daysAgo VARCHAR(255) NOT NULL,"+
+			"url TEXT NOT NULL, " +
+			"snippet TEXT NOT NULL)";
+			mysql.pool.query(createString, function(err){
+			  context.results = "Job Table reset";
+			  //can't remember what I'm doing here - getting errors when it errors ; p
+			  //indRes.setHeader('Content-Type', 'application/json');
+			  //indRes.send(context);
+			})
+		  });
+		currentJob = 1;
+		
+		while(startJob <= currentMaxJob){
+			for(i = 0; i < jobsPerPage; i++){
+				console.log("Current Job: " + currentJob)
+				//just for testing
+				console.log("jobtitle: " + jobs.results[i].jobtitle);
+					// disabled mysql due to errors
+					//basically the insert function needs to go here
+					
+					/*mysql.pool.query("INSERT INTO indeedJobs (jobTitle, company, daysAgo, snippet) VALUES (?, ?, ?, ?, ?)", [jobs.results[i].jobtitle, jobs.results[i].company, jobs.results[i].formattedRelativeTime, jobs.results[i].url, jobs.results[i].snippet],
+					function(err, result){
+						if(err){
+							next(err);
+							return;
+						}
+				
+					});*/
+					currentJob++;
+				}
+				
+				startJob += jobsPerPage;
+		}
+	});
+}
 
 var server = app.listen(app.get('port'), function() {
     console.log("Server Running.");
-
+	scrapeIndeed();
 })
